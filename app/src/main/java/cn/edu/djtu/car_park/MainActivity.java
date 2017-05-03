9d1aeba6a37878cfbf3d54e487c51783f65bfa26
Frank <709867472@q.com>
2017-05-03 16:48:09 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/djtu/car_park/MainActivity.java
@@ -0,0 +1,399 @@
+package cn.edu.djtu.car_park;
+
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.LatLngBounds;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.AMap.OnCameraChangeListener;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+import com.amap.api.services.poisearch.PoiSearch.OnPoiSearchListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.djtu.car_park.util.ToastUtil;
+
+public class MainActivity extends Activity implements OnCameraChangeListener,OnPoiSearchListener {
+    private MapView mMapView = null;
+    private AMap aMap = null;
+    // Poi查询条件类
+    private PoiSearch.Query query;
+    private PoiSearch poiSearch;
+    // poi返回的结果
+    private PoiResult poiResult;
+    // poi数据
+    private List<PoiItem> poiItems;
+    private Marker mlastMarker;
+    // poi图层
+    private MyPoiOverlay poiOverlay;
+    private RelativeLayout mPoiDetail;
+    private LatLonPoint lp = new LatLonPoint(38.913694, 121.614755);
+
+    private int[] markers = {R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+        UiSettings uiSettings = aMap.getUiSettings();
+        //隐藏logo
+        uiSettings.setLogoBottomMargin(-26);
+        //显示指南针
+        uiSettings.setCompassEnabled(true);
+        //控制比例尺控件是否显示
+        uiSettings.setScaleControlsEnabled(true);
+
+        MyLocationStyle myLocationStyle;
+        //初始化定位蓝点样式类
+        myLocationStyle = new MyLocationStyle();
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(1000);
+        //myLocationStyle.anchor(0,2);定位蓝点图标像素与定位蓝点坐标的关联点.
+        //设置定位蓝点精度圆圈的边框颜色的方法。
+        myLocationStyle.strokeColor(0x550099cc);
+        //设置定位蓝点精度圆圈的填充颜色的方法。
+        myLocationStyle.radiusFillColor(0x5500ddff);
+        //设置定位蓝点的Style
+        aMap.setMyLocationStyle(myLocationStyle);
+        //设置默认定位按钮是否显示
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setMyLocationEnabled(true);
+        //显示室内地图。
+        aMap.showIndoorMap(true);
+
+        //aMap.setMapType(AMap.MAP_TYPE_SATELLITE);设置卫星地图模式
+        //设置希望展示的地图缩放级别
+        //aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        //设置监听器
+        aMap.setOnCameraChangeListener(this);
+        //视角调整区域的中心点坐标、希望调整到的缩放级别、俯仰角0°~45°（垂直与地图时为0）、偏航角 0~360° (正北方为0)
+        CameraUpdate mCameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(38.913694, 121.614755), 15, 0, 0));
+        aMap.animateCamera(mCameraUpdate);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition position) {
+        double latitude = position.target.latitude;
+        double longitude = position.target.longitude;
+        System.out.println("纬度："+latitude + "，经度：" + longitude);
+        //视角调整区域的中心点坐标、希望调整到的缩放级别、俯仰角0°~45°（垂直与地图时为0）、偏航角 0~360° (正北方为0)
+        //CameraUpdate mCameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(38.913694, 121.614755), 15, 0, 0));
+        //aMap.animateCamera(mCameraUpdate);
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition position) {
+        // TODO Auto-generated method stub
+    }
+
+    protected void aroundSearch(){
+        /*
+        1、第一个参数表示搜索字符串，
+        2、第二个参数表示POI搜索类型，二者选填其一，
+        POI搜索类型共分为以下20种：汽车服务|汽车销售|
+        汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|医疗保健服务|
+        住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|交通设施服务|
+        金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施
+        3、第三个参数表示POI搜索区域，可以是城市编码也可以是城市名称，也可以传空字符串，空字符串代表全国在全国范围内进行搜索
+        */
+        query = new PoiSearch.Query("停车场", "", "大连");
+        // 设置每页最多返回多少条poiitem
+        query.setPageSize(10);
+        //设置查询页码
+        query.setPageNum(0);
+        if (lp != null) {
+            poiSearch = new PoiSearch(this, query);
+            poiSearch.setOnPoiSearchListener(this);
+            // 设置搜索区域为以lp点为圆心，其周围5000米范围
+            poiSearch.setBound(new PoiSearch.SearchBound(lp, 5000, true));
+            poiSearch.searchPOIAsyn();// 异步搜索
+        }
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+                    if (poiItems != null && poiItems.size() > 0) {
+                        //清除POI信息显示
+                        whetherToShowDetailInfo(false);
+                        //并还原点击marker样式
+                        if (mlastMarker != null) {
+                            resetlastmarker();
+                        }
+                        //清理之前搜索结果的marker
+                        if (poiOverlay !=null) {
+                            poiOverlay.removeFromMap();
+                        }
+                        aMap.clear();
+                        poiOverlay = new MyPoiOverlay(aMap, poiItems);
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+
+                        aMap.addMarker(new MarkerOptions()
+                                .anchor(0.5f, 0.5f)
+                                .icon(BitmapDescriptorFactory
+                                        .fromBitmap(BitmapFactory.decodeResource(
+                                                getResources(), R.drawable.point4)))
+                                .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+
+                        aMap.addCircle(new CircleOptions()
+                                .center(new LatLng(lp.getLatitude(),
+                                        lp.getLongitude())).radius(5000)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.argb(50, 1, 1, 1))
+                                .strokeWidth(2));
+
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+                        ToastUtil.show(MainActivity.this,
+                                R.string.no_result);
+                    }
+                }
+            } else {
+                ToastUtil
+                        .show(MainActivity.this, R.string.no_result);
+            }
+        } else  {
+            ToastUtil.showerror(this.getApplicationContext(), rcode);
+        }
+    }
+
+    private void whetherToShowDetailInfo(boolean isToShow) {
+        if (isToShow) {
+            mPoiDetail.setVisibility(View.VISIBLE);
+
+        } else {
+            mPoiDetail.setVisibility(View.GONE);
+
+        }
+    }
+
+    // 将之前被点击的marker置为原来的状态
+    private void resetlastmarker() {
+        int index = poiOverlay.getPoiIndex(mlastMarker);
+        if (index < 10) {
+            mlastMarker.setIcon(BitmapDescriptorFactory
+                    .fromBitmap(BitmapFactory.decodeResource(
+                            getResources(),
+                            markers[index])));
+        }else {
+            mlastMarker.setIcon(BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight)));
+        }
+        mlastMarker = null;
+
+    }
+
+    /**
+     * poi没有搜索到数据，返回一些推荐城市的信息
+     */
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+        ToastUtil.show(this, infomation);
+
+    }
+    private class MyPoiOverlay {
+        private AMap mamap;
+        private List<PoiItem> mPois;
+        private ArrayList<Marker> mPoiMarks = new ArrayList<Marker>();
+        public MyPoiOverlay(AMap amap ,List<PoiItem> pois) {
+            mamap = amap;
+            mPois = pois;
+        }
+
+        /**
+         * 添加Marker到地图中。
+         * @since V2.1.0
+         */
+        public void addToMap() {
+            for (int i = 0; i < mPois.size(); i++) {
+                Marker marker = mamap.addMarker(getMarkerOptions(i));
+                PoiItem item = mPois.get(i);
+                marker.setObject(item);
+                mPoiMarks.add(marker);
+            }
+        }
+
+        /**
+         * 去掉PoiOverlay上所有的Marker。
+         *
+         * @since V2.1.0
+         */
+        public void removeFromMap() {
+            for (Marker mark : mPoiMarks) {
+                mark.remove();
+            }
+        }
+
+        /**
+         * 移动镜头到当前的视角。
+         * @since V2.1.0
+         */
+        public void zoomToSpan() {
+            if (mPois != null && mPois.size() > 0) {
+                if (mamap == null)
+                    return;
+                LatLngBounds bounds = getLatLngBounds();
+                mamap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            }
+        }
+
+        private LatLngBounds getLatLngBounds() {
+            LatLngBounds.Builder b = LatLngBounds.builder();
+            for (int i = 0; i < mPois.size(); i++) {
+                b.include(new LatLng(mPois.get(i).getLatLonPoint().getLatitude(),
+                        mPois.get(i).getLatLonPoint().getLongitude()));
+            }
+            return b.build();
+        }
+
+        private MarkerOptions getMarkerOptions(int index) {
+            return new MarkerOptions()
+                    .position(
+                            new LatLng(mPois.get(index).getLatLonPoint()
+                                    .getLatitude(), mPois.get(index)
+                                    .getLatLonPoint().getLongitude()))
+                    .title(getTitle(index)).snippet(getSnippet(index))
+                    .icon(getBitmapDescriptor(index));
+        }
+
+        protected String getTitle(int index) {
+            return mPois.get(index).getTitle();
+        }
+
+        protected String getSnippet(int index) {
+            return mPois.get(index).getSnippet();
+        }
+
+        /**
+         * 从marker中得到poi在list的位置。
+         *
+         * @param marker 一个标记的对象。
+         * @return 返回该marker对应的poi在list的位置。
+         * @since V2.1.0
+         */
+        public int getPoiIndex(Marker marker) {
+            for (int i = 0; i < mPoiMarks.size(); i++) {
+                if (mPoiMarks.get(i).equals(marker)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /**
+         * 返回第index的poi的信息。
+         * @param index 第几个poi。
+         * @return poi的信息。poi对象详见搜索服务模块的基础核心包（com.amap.api.services.core）中的类 <strong><a href="../../../../../../Search/com/amap/api/services/core/PoiItem.html" title="com.amap.api.services.core中的类">PoiItem</a></strong>。
+         * @since V2.1.0
+         */
+        public PoiItem getPoiItem(int index) {
+            if (index < 0 || index >= mPois.size()) {
+                return null;
+            }
+            return mPois.get(index);
+        }
+
+        protected BitmapDescriptor getBitmapDescriptor(int arg0) {
+            if (arg0 < 10) {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), markers[arg0]));
+                return icon;
+            }else {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight));
+                return icon;
+            }
+        }
+    }
+}
