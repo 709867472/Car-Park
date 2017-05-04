aef69563a2ce52cb6533e5fbbd37eff60e7bd328
Frank <709867472@q.com>
2017-05-04 18:44:32 +0800

    search

--- app/src/main/java/cn/edu/djtu/car_park/MainActivity.java
+++ app/src/main/java/cn/edu/djtu/car_park/MainActivity.java
@@ -5,23 +5,28 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.AMap.InfoWindowAdapter;
+import com.amap.api.maps.AMap.OnInfoWindowClickListener;
+import com.amap.api.maps.AMap.OnMapClickListener;
+import com.amap.api.maps.AMap.OnMarkerClickListener;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
-import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
-import com.amap.api.maps.AMap.OnCameraChangeListener;
 import com.amap.api.services.core.AMapException;
 import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.core.PoiItem;
@@ -29,156 +34,162 @@ import com.amap.api.services.core.SuggestionCity;
 import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
 import com.amap.api.services.poisearch.PoiSearch.OnPoiSearchListener;
+import com.amap.api.services.poisearch.PoiSearch.SearchBound;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.edu.djtu.car_park.util.ToastUtil;
 
-public class MainActivity extends Activity implements OnCameraChangeListener,OnPoiSearchListener {
-    private MapView mMapView = null;
-    private AMap aMap = null;
-    // Poi查询条件类
-    private PoiSearch.Query query;
-    private PoiSearch poiSearch;
-    // poi返回的结果
-    private PoiResult poiResult;
-    // poi数据
-    private List<PoiItem> poiItems;
-    private Marker mlastMarker;
-    // poi图层
-    private MyPoiOverlay poiOverlay;
-    private RelativeLayout mPoiDetail;
-    private LatLonPoint lp = new LatLonPoint(38.913694, 121.614755);
+/**
+ * 介绍poi周边搜索功能
+ */
+public class MainActivity extends Activity implements OnClickListener,
+        OnMapClickListener, OnInfoWindowClickListener, InfoWindowAdapter, OnMarkerClickListener,
+        OnPoiSearchListener {
+    private MapView mapview;
+    private AMap mAMap;
 
-    private int[] markers = {R.drawable.poi_marker_1,
-            R.drawable.poi_marker_2,
-            R.drawable.poi_marker_3,
-            R.drawable.poi_marker_4,
-            R.drawable.poi_marker_5,
-            R.drawable.poi_marker_6,
-            R.drawable.poi_marker_7,
-            R.drawable.poi_marker_8,
-            R.drawable.poi_marker_9,
-            R.drawable.poi_marker_10
-    };
+    private PoiResult poiResult; // poi返回的结果
+    private int currentPage = 0;// 当前页面，从0开始计数
+    private PoiSearch.Query query;// Poi查询条件类
+    private LatLonPoint lp = new LatLonPoint(38.913694, 121.614755);// 大连坐标
+    private Marker locationMarker; // 选择的点
+    private Marker detailMarker;
+    private Marker mlastMarker;
+    private PoiSearch poiSearch;
+    private MyPoiOverlay poiOverlay;// poi图层
+    private List<PoiItem> poiItems;// poi数据
+
+    private RelativeLayout mPoiDetail;
+    private TextView mPoiName, mPoiAddress;
+    private String keyWord = "";
+    private EditText mSearchText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
-        mMapView.onCreate(savedInstanceState);
+        mapview = (MapView) findViewById(R.id.mapView);
+        mapview.onCreate(savedInstanceState);
+        init();
+    }
 
-        if (aMap == null) {
-            aMap = mMapView.getMap();
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (mAMap == null) {
+            mAMap = mapview.getMap();
+            UiSettings uiSettings = mAMap.getUiSettings();
+            uiSettings.setLogoBottomMargin(-26);//隐藏logo
+
+//            uiSettings.setCompassEnabled(true);//显示指南针
+            uiSettings.setScaleControlsEnabled(true);//控制比例尺控件是否显示
+            MyLocationStyle myLocationStyle;
+            myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类
+            myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//定位1次
+//            myLocationStyle.interval(1000);//设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+            //myLocationStyle.anchor(0,2);//定位蓝点图标像素与定位蓝点坐标的关联点.
+            myLocationStyle.strokeColor(0x550099cc);//设置定位蓝点精度圆圈的边框颜色的方法。
+            myLocationStyle.radiusFillColor(0x5500ddff);//设置定位蓝点精度圆圈的填充颜色的方法。
+            mAMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+            mAMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示
+            mAMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+            mAMap.showIndoorMap(true);//显示室内地图。
+
+            mAMap.setOnMapClickListener(this);
+            mAMap.setOnMarkerClickListener(this);
+            mAMap.setOnInfoWindowClickListener(this);
+            mAMap.setInfoWindowAdapter(this);
+            TextView searchButton = (TextView) findViewById(R.id.btn_search);
+            searchButton.setOnClickListener(this);
+
+            locationMarker = mAMap.addMarker(new MarkerOptions()
+                    .anchor(0.5f, 0.5f)
+                    .icon(BitmapDescriptorFactory
+                            .fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.point4)))
+                    .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+            locationMarker.showInfoWindow();
         }
-        UiSettings uiSettings = aMap.getUiSettings();
-        //隐藏logo
-        uiSettings.setLogoBottomMargin(-26);
-        //显示指南针
-        uiSettings.setCompassEnabled(true);
-        //控制比例尺控件是否显示
-        uiSettings.setScaleControlsEnabled(true);
-
-        MyLocationStyle myLocationStyle;
-        //初始化定位蓝点样式类
-        myLocationStyle = new MyLocationStyle();
-        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
-        //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.interval(1000);
-        //myLocationStyle.anchor(0,2);定位蓝点图标像素与定位蓝点坐标的关联点.
-        //设置定位蓝点精度圆圈的边框颜色的方法。
-        myLocationStyle.strokeColor(0x550099cc);
-        //设置定位蓝点精度圆圈的填充颜色的方法。
-        myLocationStyle.radiusFillColor(0x5500ddff);
-        //设置定位蓝点的Style
-        aMap.setMyLocationStyle(myLocationStyle);
-        //设置默认定位按钮是否显示
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);
-        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        aMap.setMyLocationEnabled(true);
-        //显示室内地图。
-        aMap.showIndoorMap(true);
-
-        //aMap.setMapType(AMap.MAP_TYPE_SATELLITE);设置卫星地图模式
-        //设置希望展示的地图缩放级别
-        //aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-
-        //设置监听器
-        aMap.setOnCameraChangeListener(this);
-        //视角调整区域的中心点坐标、希望调整到的缩放级别、俯仰角0°~45°（垂直与地图时为0）、偏航角 0~360° (正北方为0)
-        CameraUpdate mCameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(38.913694, 121.614755), 15, 0, 0));
-        aMap.animateCamera(mCameraUpdate);
+        setup();
+        mAMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lp.getLatitude(), lp.getLongitude()), 14));
     }
+    private void setup() {
+        mPoiDetail = (RelativeLayout) findViewById(R.id.poi_detail);
+        mPoiDetail.setOnClickListener(new OnClickListener() {
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        mMapView.onDestroy();
+            @Override
+            public void onClick(View v) {
+//				Intent intent = new Intent(PoiSearchActivity.this,
+//						SearchDetailActivity.class);
+//				intent.putExtra("poiitem", mPoi);
+//				startActivity(intent);
+
+            }
+        });
+        mPoiName = (TextView) findViewById(R.id.poi_name);
+        mPoiAddress = (TextView) findViewById(R.id.poi_address);
+        mSearchText = (EditText)findViewById(R.id.input_edittext);
     }
+    /**
+     * 开始进行poi搜索
+     */
+    /**
+     * 开始进行poi搜索
+     */
+    protected void doSearchQuery() {
+        keyWord = mSearchText.getText().toString().trim();
+        currentPage = 0;
+        query = new PoiSearch.Query(keyWord, "", "");// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setPageSize(20);// 设置每页最多返回多少条poiitem
+        query.setPageNum(currentPage);// 设置查第一页
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
-        mMapView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
-        mMapView.onPause();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
-        mMapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onCameraChange(CameraPosition position) {
-        double latitude = position.target.latitude;
-        double longitude = position.target.longitude;
-        System.out.println("纬度："+latitude + "，经度：" + longitude);
-        //视角调整区域的中心点坐标、希望调整到的缩放级别、俯仰角0°~45°（垂直与地图时为0）、偏航角 0~360° (正北方为0)
-        //CameraUpdate mCameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(38.913694, 121.614755), 15, 0, 0));
-        //aMap.animateCamera(mCameraUpdate);
-    }
-
-    @Override
-    public void onCameraChangeFinish(CameraPosition position) {
-        // TODO Auto-generated method stub
-    }
-
-    protected void aroundSearch(){
-        /*
-        1、第一个参数表示搜索字符串，
-        2、第二个参数表示POI搜索类型，二者选填其一，
-        POI搜索类型共分为以下20种：汽车服务|汽车销售|
-        汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|医疗保健服务|
-        住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|交通设施服务|
-        金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施
-        3、第三个参数表示POI搜索区域，可以是城市编码也可以是城市名称，也可以传空字符串，空字符串代表全国在全国范围内进行搜索
-        */
-        query = new PoiSearch.Query("停车场", "", "大连");
-        // 设置每页最多返回多少条poiitem
-        query.setPageSize(10);
-        //设置查询页码
-        query.setPageNum(0);
         if (lp != null) {
             poiSearch = new PoiSearch(this, query);
             poiSearch.setOnPoiSearchListener(this);
-            // 设置搜索区域为以lp点为圆心，其周围5000米范围
-            poiSearch.setBound(new PoiSearch.SearchBound(lp, 5000, true));
+            poiSearch.setBound(new SearchBound(lp, 5000, true));// 设置搜索区域为以lp点为圆心，其周围5000米范围
             poiSearch.searchPOIAsyn();// 异步搜索
         }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapview.onResume();
+        whetherToShowDetailInfo(false);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapview.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapview.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapview.onDestroy();
     }
 
     @Override
@@ -186,6 +197,7 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
         // TODO Auto-generated method stub
 
     }
+
 
     @Override
     public void onPoiSearched(PoiResult result, int rcode) {
@@ -207,19 +219,19 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
                         if (poiOverlay !=null) {
                             poiOverlay.removeFromMap();
                         }
-                        aMap.clear();
-                        poiOverlay = new MyPoiOverlay(aMap, poiItems);
+                        mAMap.clear();
+                        poiOverlay = new MyPoiOverlay(mAMap, poiItems);
                         poiOverlay.addToMap();
                         poiOverlay.zoomToSpan();
 
-                        aMap.addMarker(new MarkerOptions()
+                        mAMap.addMarker(new MarkerOptions()
                                 .anchor(0.5f, 0.5f)
                                 .icon(BitmapDescriptorFactory
                                         .fromBitmap(BitmapFactory.decodeResource(
                                                 getResources(), R.drawable.point4)))
                                 .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
 
-                        aMap.addCircle(new CircleOptions()
+                        mAMap.addCircle(new CircleOptions()
                                 .center(new LatLng(lp.getLatitude(),
                                         lp.getLongitude())).radius(5000)
                                 .strokeColor(Color.BLUE)
@@ -243,14 +255,38 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
         }
     }
 
-    private void whetherToShowDetailInfo(boolean isToShow) {
-        if (isToShow) {
-            mPoiDetail.setVisibility(View.VISIBLE);
 
-        } else {
-            mPoiDetail.setVisibility(View.GONE);
+    @Override
+    public boolean onMarkerClick(Marker marker) {
 
+        if (marker.getObject() != null) {
+            whetherToShowDetailInfo(true);
+            try {
+                PoiItem mCurrentPoi = (PoiItem) marker.getObject();
+                if (mlastMarker == null) {
+                    mlastMarker = marker;
+                } else {
+                    // 将之前被点击的marker置为原来的状态
+                    resetlastmarker();
+                    mlastMarker = marker;
+                }
+                detailMarker = marker;
+                detailMarker.setIcon(BitmapDescriptorFactory
+                        .fromBitmap(BitmapFactory.decodeResource(
+                                getResources(),
+                                R.drawable.poi_marker_pressed)));
+
+                setPoiItemDisplayContent(mCurrentPoi);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }else {
+            whetherToShowDetailInfo(false);
+            resetlastmarker();
         }
+
+
+        return true;
     }
 
     // 将之前被点击的marker置为原来的状态
@@ -269,6 +305,78 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
 
     }
 
+
+    private void setPoiItemDisplayContent(final PoiItem mCurrentPoi) {
+        mPoiName.setText(mCurrentPoi.getTitle());
+        mPoiAddress.setText(mCurrentPoi.getSnippet() + mCurrentPoi.getDistance());
+    }
+
+
+    @Override
+    public View getInfoContents(Marker arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public void onInfoWindowClick(Marker arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_search:
+                doSearchQuery();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private int[] markers = {R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10
+    };
+
+    private void whetherToShowDetailInfo(boolean isToShow) {
+        if (isToShow) {
+            mPoiDetail.setVisibility(View.VISIBLE);
+
+        } else {
+            mPoiDetail.setVisibility(View.GONE);
+
+        }
+    }
+
+
+    @Override
+    public void onMapClick(LatLng arg0) {
+        whetherToShowDetailInfo(false);
+        if (mlastMarker != null) {
+            resetlastmarker();
+        }
+    }
+
     /**
      * poi没有搜索到数据，返回一些推荐城市的信息
      */
@@ -282,6 +390,13 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
         ToastUtil.show(this, infomation);
 
     }
+
+
+    /**
+     * 自定义PoiOverlay
+     *
+     */
+
     private class MyPoiOverlay {
         private AMap mamap;
         private List<PoiItem> mPois;
@@ -396,4 +511,5 @@ public class MainActivity extends Activity implements OnCameraChangeListener,OnP
             }
         }
     }
+
 }
